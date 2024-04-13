66bdd50676a4e411f7644d1dae58ff49ca4faea5
mjs2435 <mjs2435@columbia.edu>
2024-04-13 15:36:50 -0400

    Did Random Forest analysis, about 70% accuracy

--- ModelCreation.R
+++ ModelCreation.R
@@ -1,10 +1,33 @@
+library(caret)
+
+library(tidymodels)
+
+library(tidyverse)
+
+library(randomForest)
+
 # Random Forest Training and Evaluation
 
+transformed_train$TARGET[transformed_train$TARGET == 0] = 'n'
+transformed_train$TARGET[transformed_train$TARGET == 1] = 'p'
+transformed_train$TARGET = as.factor(transformed_train$TARGET)
+
+transformed_test$TARGET[transformed_test$TARGET == 0] = 'n'
+transformed_test$TARGET[transformed_test$TARGET == 1] = 'p'
+transformed_test$TARGET = as.factor(transformed_test$TARGET)
+
+tot_neg = sum(transformed_train$TARGET == 'n')
+tot_pos = sum(transformed_train$TARGET == 'p')
+
+a / (a + b)
 
 
-initial_rf <- randomForest(X16 ~., data = crx_train)
+
+initial_rf <- randomForest(TARGET ~., data = transformed_train)
 
 plot(initial_rf)
+
+
 
 
 ## Resampling strategy
@@ -14,18 +37,18 @@ resample <- trainControl(method = "cv", number = 5, classProbs = TRUE, summaryFu
 
 ## Grid of hyperparameter values
 
-hyper_grid <- expand.grid(mtry = c(13, 15, 17, 20, 22, 25),
+hyper_grid <- expand.grid(mtry = c(13, 15, 17, 20, 22, 25), 
                           
-                          splitrule = c("gini", "extratrees", "hellinger"),
+                          splitrule = c("gini", "extratrees", "hellinger"), # 
                           
-                          min.node.size = c(5, 7, 9))
+                          min.node.size = c(5, 7, 9)) # 
 
 
 ## Tuning Hyperparameters
 
-rf_fit <- train(X16 ~ .,
+rf_fit <- train(TARGET ~ .,
                 
-                data = crx_train, 
+                data = transformed_train, 
                 
                 method = "ranger", 
                 
@@ -35,21 +58,26 @@ rf_fit <- train(X16 ~ .,
                 
                 metric = "ROC",
                 
-                num.trees = 250)
+                num.trees = 50)
 
 
 ggplot(rf_fit)
 
 plot(rf_fit, metric = "ROC", plotType = "level")
+plot(rf_fit, metric = "Sens", plotType = "level")
+plot(rf_fit, metric = "Spec", plotType = "level")
+plot(rf_fit, metric = "ROCSD", plotType = "level")
+
+# Best ROC for this set = with mtry = 17, min.node.size = 9, splitrule = "hellinger"
 
 # Part 2 - Final Model
 
 fitControl_final <- trainControl(method = "none", classProbs = TRUE, summaryFunction = twoClassSummary)   # no resampling applies to the data
 
 
-RF_final <- train(X16 ~., 
+RF_final <- train(TARGET ~., 
                   
-                  data = crx_train,
+                  data = transformed_train,
                   
                   method = "ranger",
                   
@@ -57,21 +85,100 @@ RF_final <- train(X16 ~.,
                   
                   metric = "ROC",
                   
-                  tuneGrid = data.frame(mtry = 15,
+                  tuneGrid = data.frame(mtry = 17,
                                         
-                                        min.node.size = 7,
+                                        min.node.size = 9,
                                         
                                         splitrule = "hellinger"),
                   
                   num.trees = 250)
 
-rf_pred_train <- predict(RF_final, newdata = crx_train)
+rf_pred_train <- predict(RF_final, newdata = transformed_train)
 
-rf_pred_test <- predict(RF_final, newdata = crx_test)
+rf_pred_test <- predict(RF_final, newdata = transformed_test)
 
-confusionMatrix(data=rf_pred_train, reference=crx_train$X16)
+confusionMatrix(data=rf_pred_train, reference=transformed_train$TARGET)
 
-confusionMatrix(data=rf_pred_test, reference=crx_test$X16)
+confusionMatrix(data=rf_pred_test, reference=transformed_test$TARGET)
+
+#### Again, but this time with a balanced dataset (through downsampling) to see if it makes a difference ####
+
+tt=transformed_train
+all_neg = tt[tt$TARGET == 'n',]
+all_pos = tt[tt$TARGET == 'p',]
+subs_neg = all_neg[sample(tot_neg, tot_pos),]
+bal_train = rbind(subs_neg, all_pos)
+
+# run training/testing again, but with bal_train
+
+initial_rf_bal <- randomForest(TARGET ~., data = bal_train)
+
+plot(initial_rf_bal)
+
+# about 200 is the best
+
+## Tuning Hyperparameters
+
+rf_fit_bal <- train(TARGET ~ .,
+                
+                data = bal_train, 
+                
+                method = "ranger", 
+                
+                trControl = resample, 
+                
+                tuneGrid = hyper_grid,
+                
+                metric = "ROC",
+                
+                num.trees = 200)
+
+
+ggplot(rf_fit_bal)
+
+plot(rf_fit_bal, metric = "ROC", plotType = "level")
+plot(rf_fit_bal, metric = "Sens", plotType = "level")
+plot(rf_fit_bal, metric = "Spec", plotType = "level")
+plot(rf_fit_bal, metric = "ROCSD", plotType = "level")
+
+
+
+fitControl_final_bal <- trainControl(method = "none", classProbs = TRUE, summaryFunction = twoClassSummary)   # no resampling applies to the data
+
+
+RF_final_bal <- train(TARGET ~., 
+                  
+                  data = bal_train,
+                  
+                  method = "ranger",
+                  
+                  trControl = fitControl_final_bal,
+                  
+                  metric = "ROC",
+                  
+                  tuneGrid = data.frame(mtry = 13,
+                                        
+                                        min.node.size = 9,
+                                        
+                                        splitrule = "hellinger"),
+                  
+                  num.trees = 200)
+
+rf_pred_train_bal_actual <- predict(RF_final_bal, newdata = bal_train)
+
+rf_pred_train_bal <- predict(RF_final_bal, newdata = transformed_train)
+
+rf_pred_test_bal <- predict(RF_final_bal, newdata = transformed_test)
+
+confusionMatrix(data=rf_pred_train_bal_actual, reference=bal_train$TARGET)
+
+confusionMatrix(data=rf_pred_train_bal, reference=transformed_train$TARGET)
+
+confusionMatrix(data=rf_pred_test_bal, reference=transformed_test$TARGET)
+
+
+
+
 
 
 # XGBoost Training and Evaluation
---
+++ get_main_data.R
@@ -0,0 +1,111 @@
+library(ggplot2)
+
+library(caret)
+
+library(tidymodels)
+
+library(tidyverse)
+
+library(SmartEDA)
+
+library(gplots)
+
+## EDA ##
+
+cc_fraud = read.csv("C:/Users/mjstr/Desktop/Git Repository Final Python Project/FinalAML/application_data.csv")
+
+summary(cc_fraud)
+
+location_categories = data.frame(matrix(ncol=3, nrow=12))
+colnames(location_categories) = c("group", "target", "count")
+
+categories = c("REG_REGION_NOT_LIVE_REGION", "REG_REGION_NOT_WORK_REGION", "LIVE_REGION_NOT_WORK_REGION", 
+               "REG_CITY_NOT_LIVE_CITY", "REG_CITY_NOT_WORK_CITY", "LIVE_CITY_NOT_WORK_CITY")
+
+i = 1
+for (category in categories) {
+  filtered_data = filter(cc_fraud, !!as.symbol(category) == 1)
+  location_categories[i,] = c(category, "0", sum(filtered_data$TARGET == 0))
+  location_categories[i+1,] = c(category, "1", sum(filtered_data$TARGET == 1))
+  i = i + 2
+}
+
+location_categories$target = as.factor(location_categories$target)
+location_categories$count = as.numeric(location_categories$count)
+
+
+
+# Create quantile functions
+quantile_25 = function(x){
+  quantile_25 = quantile(x, na.rm = TRUE, 0.1)
+}
+
+quantile_75 = function(x){
+  quantile_75 = quantile(x, na.rm = TRUE, 0.9)
+}
+
+
+## Feature Engineering ##
+
+# Noticed that "Academic degree" is a rare value for feature NAME_EDUCATION_TYPE so lumping it with "Higher education"
+cc_fraud$NAME_EDUCATION_TYPE[cc_fraud$NAME_EDUCATION_TYPE=="Academic degree"] = "Higher education"
+
+
+# Converting "XNA" values to NA
+cc_fraud[cc_fraud=="XNA"] = NA
+
+# Converting "" values to NA
+cc_fraud[cc_fraud==""] = NA
+
+# Identifying how many NA values we have in each feature
+num_nas = sort(colSums(is.na(cc_fraud)), decreasing = TRUE)
+
+# Dropping features that are missing > ~10% of values
+drop_features = names(which(num_nas > 30000))
+cc_fraud = cc_fraud %>% select(-drop_features)
+
+## Data splitting ##
+set.seed(1)
+
+# Removing loan ID 
+cc_fraud = cc_fraud %>% select(-"SK_ID_CURR")
+
+# 70%-30% split
+split = initial_split(cc_fraud, prop = 0.70, strata = "TARGET")   
+
+cc_train = training(split)
+
+cc_test = testing(split)
+
+## Data pre-processing ##
+
+# Convert all character features to factors
+# Remove all NZV features
+# Impute KNN for remaining missing values 
+# Center and scale numeric predictors
+# Pool infrequently occurring values into category called "other" (applies to NAME_TYPE_SUITE, NAME_INCOME_TYPE, NAME_HOUSING_TYPE)
+# Dummy encode all categorical features
+# Principal component analysis for numeric predictors(?)
+blueprint = recipe(TARGET ~ ., data = cc_train) %>%
+  
+  step_string2factor(all_nominal_predictors()) %>%
+  
+  step_nzv(all_predictors()) %>%
+  
+  step_impute_knn(all_predictors()) %>%
+  
+  step_center(all_numeric_predictors()) %>%
+  
+  step_scale(all_numeric_predictors()) %>%
+  
+  step_other(NAME_TYPE_SUITE, NAME_INCOME_TYPE, NAME_HOUSING_TYPE, threshold = 0.01, other = "other") %>%
+  
+  step_dummy(all_nominal())
+
+# Estimating blueprint parameters 
+blueprint_prep = prep(blueprint, training = cc_train)
+
+# Transforming data
+transformed_train = bake(blueprint_prep, new_data = cc_train)
+
+transformed_test = bake(blueprint_prep, new_data = cc_test)
