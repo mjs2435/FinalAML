cd0e95ce4335c459da4193ac9f56f08a82bdba61
mjs2435 <mjs2435@columbia.edu>
2024-04-19 11:25:19 -0400

    Functionalized some models

--- divided_model_code/ANN.R
+++ divided_model_code/ANN.R
@@ -1,64 +1,68 @@
 # Artificial Neural Network Model Training and Evaluation
+train_ANN <- function(train, grid){
+  ## Resampling strategy
+  
+  resample <- trainControl(method = "cv", number = 5, classProbs = TRUE, summaryFunction = twoClassSummary)
+  
+  
+  ## Grid of hyperparameter values
+  
+  # rang = c(0.1, 0.9),  
+  # algorithm = "rprop+"
+  
+  ## Tuning Hyperparameters
+  
+  ann_fit <- train(TARGET ~ .,
+                   
+                   data = train, 
+                   
+                   method = "nnet", 
+                   
+                   trControl = resample, 
+                   
+                   tuneGrid = grid,
+                   
+                   metric = "ROC")
+  
+  
+  ggplot(ann_fit)
+  
+  plot(ann_fit, metric = "ROC", plotType = "level")
+  plot(ann_fit, metric = "Sens", plotType = "level")
+  plot(ann_fit, metric = "Spec", plotType = "level")
+  plot(ann_fit, metric = "ROCSD", plotType = "level")
+  return(ann_fit)
+}
+final_ANN_mod <- function(train, test, grid_best){
+  
 
-
-## Resampling strategy
-
-resample <- trainControl(method = "cv", number = 5, classProbs = TRUE, summaryFunction = twoClassSummary)
-
-
-## Grid of hyperparameter values
-
+  fitControl_final <- trainControl(method = "none", classProbs = TRUE, summaryFunction = twoClassSummary)   # no resampling applies to the data
+  
+  
+  ANN_final <- train(TARGET ~., 
+                     
+                     data = train,
+                     
+                     method = "nnet",
+                     
+                     trControl = fitControl_final,
+                     
+                     metric = "ROC",
+                     
+                     tuneGrid = grid_best
+  )
+  
+  ANN_pred_train <- predict(ANN_final, newdata = train)
+  
+  ANN_pred_test <- predict(ANN_final, newdata = test)
+  
+  confusionMatrix(data=ANN_pred_train, reference=train$TARGET)
+  
+  confusionMatrix(data=ANN_pred_test, reference=test$TARGET)
+  return(ANN_final)
+}
 grid <- expand.grid(size = c(2, 5, 10, 15),
                     decay = c(0, 0.1, 0.05, 0.01))  
-# rang = c(0.1, 0.9),  
-# algorithm = "rprop+"
-
-## Tuning Hyperparameters
-
-ann_fit <- train(TARGET ~ .,
-                 
-                 data = transformed_train, 
-                 
-                 method = "nnet", 
-                 
-                 trControl = resample, 
-                 
-                 tuneGrid = grid,
-                 
-                 metric = "ROC")
-
-
-ggplot(ann_fit)
-
-plot(ann_fit, metric = "ROC", plotType = "level")
-plot(ann_fit, metric = "Sens", plotType = "level")
-plot(ann_fit, metric = "Spec", plotType = "level")
-plot(ann_fit, metric = "ROCSD", plotType = "level")
-# use Hidden = 10, Weight Decay = 0.1
-
-fitControl_final <- trainControl(method = "none", classProbs = TRUE, summaryFunction = twoClassSummary)   # no resampling applies to the data
-
-
-ANN_final <- train(TARGET ~., 
-                   
-                   data = transformed_train,
-                   
-                   method = "nnet",
-                   
-                   trControl = fitControl_final,
-                   
-                   metric = "ROC",
-                   
-                   tuneGrid = data.frame(size = 15,
-                                         
-                                         decay = 0
-                   )
-)
-
-ANN_pred_train <- predict(ANN_final, newdata = transformed_train)
-
-ANN_pred_test <- predict(ANN_final, newdata = transformed_test)
-
-confusionMatrix(data=ANN_pred_train, reference=transformed_train$TARGET)
-
-confusionMatrix(data=ANN_pred_test, reference=transformed_test$TARGET)
+grid_best = data.frame(size = 15, decay = 0)
+ANN_tr = train_ANN(transformed_train, grid)
+ANN_fn = final_ANN_mod(transformed_train, transformed_test, grid_best)
--- divided_model_code/XGB.R
+++ divided_model_code/XGB.R
@@ -2,84 +2,76 @@
 
 # XGBoost Training and Evaluation
 
-resample <- trainControl(method = "cv",
-                         
-                         number = 5,
-                         
-                         classProbs = TRUE,
-                         
-                         summaryFunction = twoClassSummary) 
+train_XGB <- function(train, grid){
+  resample <- trainControl(method = "cv",
+                           number = 5,
+                           classProbs = TRUE,
+                           summaryFunction = twoClassSummary)
+  
+  xg_fit <- train(TARGET ~ .,
+                  data = train, 
+                  method = "xgbTree",
+                  trControl = resample,
+                  tuneGrid = grid,
+                  metric = "ROC")
+  ggplot(xg_fit)
+  
+  plot(xg_fit, metric = "ROC", plotType = "level")
+  plot(xg_fit, metric = "Sens", plotType = "level")
+  plot(xg_fit, metric = "Spec", plotType = "level")
+  plot(xg_fit, metric = "ROCSD", plotType = "level")
+  
+  return(xg_fit)
+  
+}
 
-
-
-hyper_grid <- expand.grid(nrounds = c(300, 400),   
-                          
-                          max_depth = c(1, 2, 4), 
-                          
-                          eta = c(0.05, 0.01),    
-                          
-                          min_child_weight = c(15), 
-                          
-                          subsample = 0.6, 
-                          
-                          gamma = 0,
-                          
-                          colsample_bytree = 1)
-
-
-
-xg_fit <- train(TARGET ~ .,
-                
-                data = transformed_train, 
-                
-                method = "xgbTree",
-                
-                trControl = resample,
-                
-                tuneGrid = hyper_grid,
-                
-                metric = "ROC")
-ggplot(xg_fit)
-
-plot(xg_fit, metric = "ROC", plotType = "level")
-plot(xg_fit, metric = "Sens", plotType = "level")
-plot(xg_fit, metric = "Spec", plotType = "level")
-plot(xg_fit, metric = "ROCSD", plotType = "level")
 
 # Final Model
+final_XGB_mod <- function(train, test, best_grid){
+  fitControl_final <- trainControl(method = "none", classProbs = TRUE, summaryFunction = twoClassSummary)   # no resampling applies to the data
+  
+  
+  XG_final <- train(TARGET ~., 
+                    
+                    data = train,
+                    
+                    method = "xgbTree",
+                    
+                    trControl = fitControl_final,
+                    
+                    metric = "ROC",
+                    
+                    tuneGrid = best_grid
+  )
+  
+  xg_pred_train <- predict(XG_final, newdata = train)
+  
+  xg_pred_test <- predict(XG_final, newdata = test)
+  
+  confusionMatrix(data=xg_pred_train, reference=train$TARGET)
+  
+  confusionMatrix(data=xg_pred_test, reference=test$TARGET)
+  
+  return(XG_final)
 
-fitControl_final <- trainControl(method = "none", classProbs = TRUE, summaryFunction = twoClassSummary)   # no resampling applies to the data
+}
 
+grid <- expand.grid(nrounds = c(300, 400),   
+                          max_depth = c(1, 2, 4), 
+                          eta = c(0.05, 0.01),    
+                          min_child_weight = c(15), 
+                          subsample = 0.6, 
+                          gamma = 0,
+                          colsample_bytree = 1)
 
-XG_final <- train(TARGET ~., 
-                  
-                  data = transformed_train,
-                  
-                  method = "xgbTree",
-                  
-                  trControl = fitControl_final,
-                  
-                  metric = "ROC",
-                  
-                  tuneGrid = data.frame(nrounds = 400,
-                                        
-                                        max_depth = 4,
-                                        
-                                        eta = 0.05,
-                                        
-                                        gamma = 0,
-                                        
-                                        colsample_bytree = 1,
-                                        
-                                        min_child_weight = 15,
-                                        
-                                        subsample = .6)
-)
+best_grid = data.frame(nrounds = 400,
+           max_depth = 4,
+           eta = 0.05,
+           gamma = 0,
+           colsample_bytree = 1,
+           min_child_weight = 15,
+           subsample = .6)
 
-xg_pred_train <- predict(XG_final, newdata = transformed_train)
+train_XGB(transformed_train, grid)
 
-xg_pred_test <- predict(XG_final, newdata = transformed_test)
-
-confusionMatrix(data=xg_pred_train, reference=transformed_train$TARGET)
-
-confusionMatrix(data=xg_pred_test, reference=transformed_test$TARGET)
+final_XGB_mod(transformed_train, transformed_test, best_grid)
