9ccf7763dabe299ace52fb3e7ba1fb5ec3daa198
Auroaruora <zw2985@columbia.edu>
2024-05-01 15:22:12 -0400

    adding testing.R for a condense pipeline

--- testing.R
+++ testing.R
@@ -23,11 +23,14 @@ library(shinyjs)
 setwd("/Users/auroraweng/Desktop/STAT3106 AML/FinalAML/")
 data <- read.csv("Credit_Card_Fraud_Detection/data/subset_application_data.csv", header = TRUE)
 set.seed(50)
+
 split <- initial_split(data, prop = 0.70, strata = "TARGET")   # 70%-30% split
 train_data <- training(split)
 test_data <- testing(split)
+
 train_data$TARGET <- factor(train_data$TARGET, levels = c("0", "1"),labels = c("Positive", "Negative"))
 test_data$TARGET <- factor(test_data$TARGET, levels = c("0", "1"),labels = c("Positive", "Negative"))
+
 blueprint <- recipe(TARGET ~., data = train_data) %>%step_string2factor(all_nominal(), -all_outcomes())
 blueprint <-blueprint %>% step_nzv(all_predictors())
 blueprint <-blueprint %>% step_impute_knn(all_predictors())
@@ -36,6 +39,7 @@ blueprint <- blueprint %>% step_center(all_numeric_predictors()) %>%
   step_scale(all_numeric_predictors())
 blueprint<- blueprint%>%step_dummy(all_nominal(), -all_outcomes())
 blueprint_prep = prep(blueprint, training = train_data)
+
 transformed_train=transformed_train<-(bake(blueprint_prep, new_data = train_data))
 transformed_test=transformed_test<-(bake(blueprint_prep, new_data = test_data))
 
@@ -51,6 +55,7 @@ hyper_grid <- expand.grid(mtry = c(13, 15, 17, 20, 22, 25),
                           splitrule = c("gini"), # 
                           
                           min.node.size = c(5, 7, 9)) # 
+
 rf_fit <- train(TARGET ~ .,
                 
                 data = transformed_train, 
